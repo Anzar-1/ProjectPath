9e02be07971515d0dfe6afd94a976a350504eef6
Anzar <tatachak8@gmail.com>
2026-05-08 21:00:44 +0100

    addingRedesign

--- ProjectPath/PP/static/js/message_admin.js
+++ ProjectPath/PP/static/js/message_admin.js
@@ -1,3 +1,14 @@
+// Gestion de la navigation
+let buttons = document.querySelectorAll("nav .same");
+buttons.forEach(btn => {
+    btn.addEventListener("click", function() {
+        let page = this.getAttribute("data-page");
+        if (page) {
+            window.location.href = page;
+        }
+    });
+});
+
 
 // === MESSAGE TABS ===
 function switchMessageTab(tabName) {
